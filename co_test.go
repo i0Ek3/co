a6cec9c82657de1dfd733d96f569347c0a893c89
i0Ek3 <kno30826@gmail.com>
2020-10-24 00:14:56 +0800

    Remove dealgo() in co_test.go

--- co_test.go
+++ co_test.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"testing"
-
-	_ "github.com/stretchr/testify/suite"
 )
 
 func TestObfuscate(t *testing.T) {
-
 	t.Run("", func(t *testing.T) {
 		c := &Confuse{"", true, 3, 8}
 		got := c.coalgo3("h")
@@ -22,26 +19,6 @@ func TestObfuscate(t *testing.T) {
 		c := &Confuse{"", true, 3, 8}
 		got := c.coalgo3("")
 		want := ""
-
-		if got != want {
-			t.Errorf("got: '%q' want: '%q'", got, want)
-		}
-	})
-	
-	t.Run("", func(t *testing.T) {
-		c := &Confuse{"", true, 3, 8}
-		got := c.dealgo3("-0_9")
-		want := "h"
-
-		if got != want {
-			t.Errorf("got: '%q' want: '%q'", got, want)
-		}
-	})
-	
-	t.Run("", func(t *testing.T) {
-		c := &Confuse{"", true, 3, 8}
-		got := c.dealgo3("h")
-		want := "-0_9"
 
 		if got != want {
 			t.Errorf("got: '%q' want: '%q'", got, want)
