2e4f2ed9f159e9843f13d0ae4cc98ffc54be7a78
i0Ek3 <kno30826@gmail.com>
2020-11-13 17:07:41 +0800

    Fix errors in co_test.go

--- co_test.go
+++ co_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestObfuscate(t *testing.T) {
-	c := &Confuse{cobit: 8, debug: false}
+	c := &Confuse{cobit: 8, debug: "false"}
 	check := func(t *testing.T, got, want string) {
 		if got != want {
 			t.Errorf("got: '%s' want: '%s'", got, want)
@@ -14,27 +14,27 @@ func TestObfuscate(t *testing.T) {
 	}
 
 	t.Run("obfuscate", func(t *testing.T) {
-		got := c.coalgo4String("hello", c.debug)
+		got := c.coalgo4("hello", c.debug)
 		want := "-0_7-0_4-1_3-1_3-1_6"
 		check(t, got, want)
 	})
 
 	t.Run("obfuscate", func(t *testing.T) {
-		got := c.coalgo4String("", c.debug)
+		got := c.coalgo4("", c.debug)
 		want := ""
 		check(t, got, want)
 	})
 }
 
 func BenchmarkObfuscate(b *testing.B) {
-	c := &Confuse{"", true, 3, 8, false}
+	c := &Confuse{"", true, 4, 8, "false"}
 	for i := 0; i < b.N; i++ {
 		c.Obfuscate("hello", c.debug, c.algoid)
 	}
 }
 
 func TestDebfuscate(t *testing.T) {
-	c := &Confuse{cobit: 8, debug: false}
+	c := &Confuse{cobit: 8, debug: "false"}
 	check := func(t *testing.T, got, want string) {
 		if got != want {
 			t.Errorf("got: '%s' want: '%s'", got, want)
@@ -55,15 +55,15 @@ func TestDebfuscate(t *testing.T) {
 }
 
 func BenchmarkDeobfuscate(b *testing.B) {
-	c := &Confuse{"", true, 3, 8, false}
+	c := &Confuse{"", true, 3, 8, "false"}
 	for i := 0; i < b.N; i++ {
 		c.Deobfuscate("-1_0-1_0-1_0", c.debug, c.algoid)
 	}
 }
 
 func ExampleObfuscate() {
-	c := &Confuse{"", true, 3, 8, false}
-	ret := c.coalgo4String("h", c.debug)
+	c := &Confuse{"", true, 3, 8, "false"}
+	ret := c.coalgo4("h", c.debug)
 	fmt.Println(ret)
 	// Output: -0_7
 }
